d7bf58dbbb608cc7f085865109c864b56d79cd00
renyufu <stnren@qq.com>
2016-08-22 13:58:57 +0800

    modified:   master.js

--- plugins/reveal/master.js
+++ plugins/reveal/master.js
@@ -11,11 +11,16 @@
 
 	};
 
-	// Monitor events that trigger a change in state
-	Reveal.addEventListener( 'slidechanged', post );
-	Reveal.addEventListener( 'fragmentshown', post );
-	Reveal.addEventListener( 'fragmenthidden', post );
-	Reveal.addEventListener( 'overviewhidden', post );
-	Reveal.addEventListener( 'overviewshown', post );
-	Reveal.addEventListener( 'paused', post );
-	Reveal.addEventListener( 'resumed', post );
+
+	// hack: prevent notes' upcomming iframe send websocket msg
+	if (Reveal.getConfig().controls)  {
+
+		// Monitor events that trigger a change in state
+		Reveal.addEventListener( 'slidechanged', post );
+		Reveal.addEventListener( 'fragmentshown', post );
+		Reveal.addEventListener( 'fragmenthidden', post );
+		Reveal.addEventListener( 'overviewhidden', post );
+		Reveal.addEventListener( 'overviewshown', post );
+		Reveal.addEventListener( 'paused', post );
+		Reveal.addEventListener( 'resumed', post );
+	}
